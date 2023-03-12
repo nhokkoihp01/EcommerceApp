9a883063e49b0f89e9c5b0705677ff3b2b51d3a0
Nguyen Minh Huy <91535034+nhokkoihp01@users.noreply.github.com>
2023-03-12 10:52:46 +0700

    category ++

---
+++ app/src/main/java/nlu/edu/vn/ecommerce/models/CategoryModel.java
@@ -0,0 +1,34 @@
+package nlu.edu.vn.ecommerce.models;
+
+public class CategoryModel {
+    private String img_url;
+    private String name;
+    private String type;
+
+    public CategoryModel() {
+    }
+
+    public String getImg_url() {
+        return img_url;
+    }
+
+    public void setImg_url(String img_url) {
+        this.img_url = img_url;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
