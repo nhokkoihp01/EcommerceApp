ee7c90dcc14889cf5616d677f999950452872e13
Nguyen Minh Huy <91535034+nhokkoihp01@users.noreply.github.com>
2023-03-15 09:58:59 +0700

    all product ++

---
+++ app/src/main/java/nlu/edu/vn/ecommerce/activities/AllProductActivity.java
@@ -0,0 +1,68 @@
+package nlu.edu.vn.ecommerce.activities;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import nlu.edu.vn.ecommerce.R;
+import nlu.edu.vn.ecommerce.adapters.AllProductAdapter;
+import nlu.edu.vn.ecommerce.models.AllProductModel;
+
+
+public class AllProductActivity extends AppCompatActivity {
+    private RecyclerView allProductRecyclerView;
+    private AllProductAdapter allProductAdapter;
+    private List<AllProductModel> allProductModels;
+    private FirebaseFirestore db;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_product);
+        mapping();
+        allProductRecyclerView.setLayoutManager(new GridLayoutManager(this,2));
+        allProductAdapter = new AllProductAdapter(this,allProductModels);
+        allProductRecyclerView.setAdapter(allProductAdapter);
+        db.collection("AllProduct")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                AllProductModel allProductModel = document.toObject(AllProductModel.class);
+                                allProductModels.add(allProductModel);
+                                allProductAdapter.notifyDataSetChanged();
+
+                            }
+                        } else {
+                            Toast.makeText(getApplicationContext(),"" + task.getException(),Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                });
+
+
+    }
+
+    private void mapping() {
+        db = FirebaseFirestore.getInstance();
+        allProductRecyclerView = findViewById(R.id.all_product_rec);
+
+        allProductModels = new ArrayList<>();
+    }
+}
